6cac6b7ee52d87c8d3fbaa81b967ae6603b76d11
PrajaktaBankar <110220028+PrajaktaBankar@users.noreply.github.com>
2023-02-02 17:05:30 +0530

    Update loop.java

--- loop.java
+++ loop.java
@@ -2,7 +2,7 @@ class loop {
 public static void main(String[] args) {  
     //Code of Java for loop  
     for(int i=1;i<=1000000;i++){  
-        System.out.println(i);  
+        System.out.println(i*i);  
     }  
 }  
 }  
